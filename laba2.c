a88a36cf9ca7aa232a88614d9131419f3ce9c9a0
llupanar <71102491+llupanar@users.noreply.github.com>
2020-09-11 22:15:51 +0300

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -3,20 +3,21 @@
 int main() {
 
 	int f = 0, s = 0, k = 0;
-	char mas[] = { " hello world " };
+	char mas[16] = { "  hello world  " };
 
-	for (int i = 0; mas[i]; i++) {
+	for (int i = 0; i<15; i++) {
 		if (mas[i] != ' ') {
 			s++;
 			if (f == 0) {
 				k++;
 				f = 1;
-	}
+			}
 		}
 		else {
 			f = 0;
 		}
 	}
+	
 	printf("%.2f", (float)s / k);
+	
 }
-
